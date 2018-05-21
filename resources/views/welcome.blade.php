8ea6d7a97c2943841f9988c72fc8b9cac3cd1710
Todor Chakarov <tosheee@abv.bg>
2018-05-21 12:10:15 +0300

    Change welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel test deploy
                 </div>
 
                 <div class="links">
